df6315e2290f5cdab7a13b371917324857e5e5af
gfpaiva <gf.paiva@yahoo.com.br>
2017-11-30 01:02:09 -0200

    feat: fetch api data, and render dinamic books

--- src/components/Book/Book.js
+++ src/components/Book/Book.js
@@ -2,12 +2,18 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 class Book extends Component {
+	constructor(props) {
+		super(props);
+
+		console.log('⌛⌛⌛⌛', props);
+	}
 	render() {
+		const {title, authors, imageLinks, shelf} = this.props;
 		return (
 			<li>
 				<div className="book">
 					<div className="book-top">
-						<div className="book-cover" style={{ width: 128, height: 193, backgroundImage: 'url("http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api")' }}></div>
+						<div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url("${imageLinks.smallThumbnail}")` }}></div>
 						<div className="book-shelf-changer">
 							<select>
 								<option value="none" disabled>Move to...</option>
@@ -18,16 +24,18 @@ class Book extends Component {
 							</select>
 						</div>
 					</div>
-					<div className="book-title">To Kill a Mockingbird</div>
-					<div className="book-authors">Harper Lee</div>
+					<div className="book-title">{title}</div>
+					<div className="book-authors">{authors[0]}</div>
 				</div>
 			</li>
 		);
 	}
 }
 
-Book.propTypes = {}
+Book.propTypes = {
+	title: PropTypes.string.isRequired,
+	authors: PropTypes.array.isRequired,
+	imageLinks: PropTypes.object.isRequired,
+}
 
-Book.defaultProps = {}
-
-export default Book
+export default Book;
--- src/components/BookShelf/BookShelf.js
+++ src/components/BookShelf/BookShelf.js
@@ -4,12 +4,20 @@ import PropTypes from 'prop-types';
 
 class BookShelf extends Component {
 	render() {
+		const { type, title, books } = this.props;
+		const currentBooks = books.filter(book => book.shelf === type);
+
 		return (
 			<div className="bookshelf">
-				<h2 className="bookshelf-title">Currently Reading</h2>
+				<h2 className="bookshelf-title">{title}</h2>
 				<div className="bookshelf-books">
 					<ol className="books-grid">
-						<Book />
+						{currentBooks.map(book => {
+							const {title, authors, imageLinks, shelf} = book;
+
+							return <Book  {...{title, authors, imageLinks}} key={book.id} />
+							}
+						)}
 					</ol>
 				</div>
 			</div>
@@ -17,8 +25,8 @@ class BookShelf extends Component {
 	};
 };
 
-BookShelf.propTypes = {};
-
-BookShelf.defaultProps = {};
+BookShelf.propTypes = {
+	title: PropTypes.string.isRequired
+};
 
 export default BookShelf;
